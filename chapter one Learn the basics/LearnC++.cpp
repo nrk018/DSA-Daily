91a5625418c70b7e2b42792d6831618790eb1297
Nirmal Rajkumar <nirmalrajkumarofficial@gmail.com>
2026-01-13 18:55:50 +0530

    day 0 update

---
+++ chapter one Learn the basics/LearnC++.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Nirmal Rajkumar on 13/01/26.
+//
+ /* #include <bits/stdc++.h> */ // includes all library at once.
+#include<iostream>
+using namespace std; // dont have to write std everytime
+
+/* //basic printing
+int main(){
+        std::cout << " heylo nig!" << "\n";
+        std::cout << " heylo again nig!" << std::endl;
+        std::cout << " hmm hi";
+        return 0;
+}
+
+*/
+
+/* // taking input
+int main() {
+        int x,y ;
+        cin>>x >>y;
+        cout << "value of x is:" << x << endl << "value of y is:" << y << endl ;
+        return 0;
+}
+*/
+/* // trying to figure out life
+int main() {
+    //int
+    int x = 10;
+    //long
+    long x = 15;
+    cin>> x;
+
+    long long x = 15000000;
+
+    return 0;
+}
+*/
+
+/* // data types
+int main() {
+
+    float x = 5.6;
+    float y = 9;
+    cout << "value of x is " << x << endl << "value of y is " << y << endl;
+    return 0;
+}
+*/
+
+int main() {
+    // int , long , long long , float , double
+    // string and getline
+    /* // returns only the word before space
+    string s;
+    cin>>s;
+    cout<<s;
+    return 0;
+    */
+    /* // this will produce out two words hence now we know string keyword recognises only one word at a time
+    string s1;
+    string s2;
+    cin>> s1 >> s2;
+    cout << s1 << " " << s2;
+    return 0;
+    */
+
+    /* getline helps to print multiple lines
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    return 0;
+
+}
+
